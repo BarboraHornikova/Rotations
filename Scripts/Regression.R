7676e05b4457aef2dc4001c70d69235a1254e887
VojtechHerrmann <herrmann.vojtech@gmail.com>
2020-04-23 02:02:28 +0200

    filter for checking

--- Scripts/Regression.R
+++ Scripts/Regression.R
@@ -1,8 +1,15 @@
 source("scripts/Import.R")
 
-m1_a_s = lm(M1_A_S ~ ServingRotation + ReceivingRotation, data = RalliesRotations)
-m2_a_s = lm(M2_A_S ~ ServingRotation + ReceivingRotation, data = RalliesRotations)
-m3_a_s = lm(M3_A_S ~ ServingRotation + ReceivingRotation, data = RalliesRotations)
+# temporary filter for check of R^2
+data = RalliesRotations %>%
+  filter((MarkovCoefLastTouchSideout > 0) & (MarkovCoefLastTouchSideout < 1))
+
+# this is correct
+# data = RalliesRotations
+
+m1_a_s = lm(M1_A_S ~ ServingRotation + ReceivingRotation, data = data)
+m2_a_s = lm(M2_A_S ~ ServingRotation + ReceivingRotation, data = data)
+m3_a_s = lm(M3_A_S ~ ServingRotation + ReceivingRotation, data = data)
 
 summary(m1_a_s)
 summary(m2_a_s)
@@ -15,3 +22,10 @@ m3_a_s_res = m3_a_s$residuals
 hist(m1_a_s_res)
 hist(m2_a_s_res)
 hist(m3_a_s_res)
+
+plot(m1_a_s)
+plot(m2_a_s)
+plot(m3_a_s)
+
+
+
